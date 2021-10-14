078185b3360c5c616886f7a43dc2257ab439ad73
Daniel Fox <61083107+dfox97@users.noreply.github.com>
2021-10-14 23:40:43 +0100

    Add files via upload

--- TodoList/tasks/forms.py
+++ TodoList/tasks/forms.py
@@ -4,8 +4,9 @@ from django.forms import ModelForm
 from .models import *
 
 class TaskForm(forms.ModelForm):
-    task_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add a task...'}))
+    task_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add a task...','class' : 'mycalss'}))
     class Meta:
         model=Task
         fields='__all__'
+       
     
